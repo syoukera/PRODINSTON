9a238099b4d6e5b2990a35c9c38639375869062b
syoukera <s.akira2986@gmail.com>
2022-03-30 14:52:58 +0900

    [fix_flame_pos] Change pos to index

--- 080_fix_flame_pos.f90
+++ 080_fix_flame_pos.f90
@@ -9,24 +9,24 @@ subroutine fix_flame_pos()
 end subroutine
 
 subroutine get_flame_pos()
-    use main_variables, only:nmax, xscl, temp, temp_flame, pos_flame
+    use main_variables, only:nmax, xscl, temp, temp_flame, n_flame
     implicit none
 
     integer n
 
     do n = 1, nmax
         if (temp(n) < temp_flame) then
-            pos_flame = xscl(n)
+            n_flame = n
             exit
         endif
 
         if (n == nmax) then
-            write (6, 'Error: Flame position was not found')
+            write (6, *) 'Error: Flame position was not found'
             stop
         endif
     enddo
 
-    ! write (6,'("flame position = ",e12.4)') pos_flame
+    write (6,'("flame position = ",e12.4)') xscl(n_flame)
 
 end subroutine
 
