3d8863db229747f6980cd54be255776b50ae2fce
syoukera <s.akira2986@gmail.com>
2022-03-30 16:46:03 +0900

    [fix_flame_pos] Add disp_flame_pos

--- 080_fix_flame_pos.f90
+++ 080_fix_flame_pos.f90
@@ -3,13 +3,13 @@ subroutine fix_flame_pos()
     implicit none
 
     call get_flame_pos
-    call calc_flame_vel
+    ! call calc_flame_vel
     call disp_flame_pos
 
 end subroutine
 
 subroutine get_flame_pos()
-    use main_variables, only:nmax, xscl, temp, temp_flame, n_flame
+    use main_variables, only: nmax, xscl, temp, temp_flame, n_flame
     implicit none
 
     integer n
@@ -36,6 +36,40 @@ subroutine calc_flame_vel()
 end subroutine
 
 subroutine disp_flame_pos()
+    use main_variables, only: nmax, nsp, n_flame, n_flame_fix, &
+                              temp, vel, pres, dens, m_chsp
     implicit none
+    
+    integer :: n, nn, i, n_diff
+    real*8 mf_chem(nsp)
+    
+    n_diff = n_flame - n_flame_fix
+
+    data mf_chem  /2.85110947E-02, 0.00000000E+00, 2.26276778E-01, 0.00000000E+00, 0.00000000E+00, &
+                   0.00000000E+00, 0.00000000E+00, 0.00000000E+00, 7.45212127E-01/
+
+    if (n_diff > 0) then
+
+        ! assign umbured mixture
+        do n = nmax-n_diff+1, nmax
+            temp(n) = 300.0d0
+            do i = 1, nsp
+                m_chsp(n, i) = mf_chem(i)
+            enddo
+        enddo
+
+        ! asign upstream (larger index) value
+        do n = 1, nmax-n_diff
+            nn = n + n_diff
+
+            temp(n) = temp(nn)
+            vel(n) = vel(nn)
+            pres(n) = pres(nn)
+            dens(n) = dens(nn)
+            do i = 1, nsp
+                m_chsp(n, i) = m_chsp(nn, i)
+            enddo
+        enddo
+    endif
 
 end subroutine
