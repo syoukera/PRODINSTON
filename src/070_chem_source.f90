de6bbd410c33c354051715c739761af69e0b1f8b
syoukera <s.akira2986@gmail.com>
2023-04-07 16:12:45 +0900

    [chem_source] Add coments on chems_source_cantera

--- src/070_chem_source.f90
+++ src/070_chem_source.f90
@@ -7,31 +7,46 @@ subroutine chem_source_cantera(ntime)
     !$ use omp_lib
 
     integer ntime
-    real*8 t_cell
-    real*8 mf(nsp),hbms
-    real*8 chem_t
+    real*8 t_cell ! working variable for temperature
+    real*8 mf(nsp),hbms 
+    real*8 chem_t ! total value of mass fraction of all species
     real*8 :: tols(4)
     data tols /1.E-8, 1.E-20, 1.E-5, 1.E-5/
 
     !$omp parallel
     do n=1,nmax
+
+        ! assignt old temperature to working variables
         t_cell = o_temp(n)
+
+        ! total value of mass fraction of all species
         chem_t=0.0d0
+
         do i=1, nsp
+            ! assign old mass fraction to working variable
             mf(i) = o_m_chsp(n,i)
-            if (mf(i).le.0.0d0) mf(i) = 0.0d0
+            ! operation not to mf less than 0.0
+            if (mf(i).le.0.0d0) then
+                mf(i) = 0.0d0
+            end if
+            ! add mass fraction to total value
             chem_t=chem_t+mf(i)
         end do
 ! 
         do i=1,nsp
+            ! normalize mf using total mass fraction
             mf(i) = mf(i)/chem_t
         end do
 !
+        ! update mass fraction and temperature usign cantera
         call getnextty(mf, t_cell, delt_t)
 !
+        ! assign updated temperature to current temperature
         temp(n) = t_cell
         do i=1, nsp
-            o_m_chsp(n,i) = mf(i)
+            ! assign updated mass fractions to old mass fraction
+            o_m_chsp(n,i) = mf(i) 
+            ! assign updated mass fractions to current mass fraction
             m_chsp(n,i)   = mf(i) 
         end do
     end do      
