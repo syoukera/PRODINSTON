658578cc796ef4d53d4d7f7d53c93d7017a481ec
syoukera <s.akira2986@gmail.com>
2023-05-25 17:23:03 +0900

    [040_scl_trans] Add mobility term

--- src/040_scl_trans.f90
+++ src/040_scl_trans.f90
@@ -2,7 +2,7 @@ subroutine scl_trans
 !
     use main_variables
     real*8 a_i(nmax), b_i(nmax), c_i(nmax), d_i(nmax)
-    real*8 phi(nmax), S_i(nmax), Gamma(nmax)
+    real*8 phi(nmax), S_i(nmax), Gamma(nmax), Beta(nmax)
     integer n_up
 !   --------- n_up: upwind switch u_up=1 ----------------
     n_up = 2
@@ -12,9 +12,10 @@ subroutine scl_trans
             phi(n)   = o_m_chsp(n,i)
             S_i(n)   = 0.0d0
             Gamma(n) = dens(n)*x_D(n,i)
+            Beta(n)  = dens(n)*mobility(n, i)*E(n)
        end do
 !
-        call calc_scl_coef(phi, Gamma, S_i, a_i, b_i, c_i, d_i,n_up)
+        call calc_scl_coef(phi, Gamma, Beta, S_i, a_i, b_i, c_i, d_i,n_up)
 !
         call calc_tdma(phi, a_i, b_i, c_i, d_i)
 !
