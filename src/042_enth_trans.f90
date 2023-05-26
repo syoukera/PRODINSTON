521a900b58b14445cddc498ea447593bcfe0c8ff
syoukera <s.akira2986@gmail.com>
2023-05-26 12:43:23 +0900

    [042_enth_trans] Apply Beta in enth_trans

--- src/042_enth_trans.f90
+++ src/042_enth_trans.f90
@@ -2,7 +2,7 @@ subroutine enth_trans
 !
     use main_variables
     real*8 a_i(nmax), b_i(nmax), c_i(nmax), d_i(nmax)
-    real*8 phi(nmax), S_i(nmax), Gamma(nmax)
+    real*8 phi(nmax), S_i(nmax), Gamma(nmax), Beta(nmax)
     integer n_up
     real*8, parameter :: C_p = 1006.0      !!temporary specific heat   (N2@300K[J/kgK]) 
 !   --------- n_up: upwind switch u_up=1 ----------------
@@ -12,9 +12,10 @@ subroutine enth_trans
         phi(n) = temp(n)
         S_i(n) = 0.0d0
         Gamma(n) = T_D(n)
+        Beta(n) = 0.0d0
     end do
 !
-    call calc_scl_coef(phi, Gamma, S_i, a_i, b_i, c_i, d_i,n_up)
+    call calc_scl_coef(phi, Gamma, Beta, S_i, a_i, b_i, c_i, d_i,n_up)
 !
     call calc_tdma(phi, a_i, b_i, c_i, d_i)
 !
