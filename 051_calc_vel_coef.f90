9895fa3a237ef88766c8efbfa730b66d212c6220
syoukera <s.akira2986@gmail.com>
2023-07-11 17:37:44 +0900

    [051_calc_vel_coef] Update n = 2, nmax-1

--- 051_calc_vel_coef.f90
+++ 051_calc_vel_coef.f90
@@ -3,15 +3,16 @@ subroutine calc_vel_coef(phi, Gamma, S_i, a_i, b_i, c_i, d_i,n_up)
 
     use main_variables
 !
-!
-    real*8 F_e, F_w
-    real*8 D_e, D_w
-    real*8 Pe_e, Pe_w, APe_e, APe_w
-    real*8 delt_x_e, delt_x_w, delt_xc_e, delt_xc_w
-    real*8 a_i(nmax), b_i(nmax), c_i(nmax), d_i(nmax)
-    real*8 phi(nmax), S_i(nmax),Gamma(nmax)
-    real*8 Gamma_e, Gamma_w, S_e, S_w, V_p
-    integer n_up
+!    
+    real*8, intent(in) :: phi(nmax), S_i(nmax),Gamma(nmax) 
+    integer, intent(in) :: n_up
+    real*8, intent(out) :: a_i(nmax), b_i(nmax), c_i(nmax), d_i(nmax)
+
+    real*8 F_E, F_P
+    real*8 D_E, D_P
+    real*8 Pe_E, Pe_P, APe_E, APe_P
+    real*8 delt_x_E, delt_x_P
+    real*8 S_E, S_P, S_sp, V_e
 !
 !   ------- n=1 center boundary condition ---------
     Gamma_e  = xvel(1)/xscl(2)*(Gamma(2)-Gamma(1))+Gamma(1)
@@ -39,52 +40,64 @@ subroutine calc_vel_coef(phi, Gamma, S_i, a_i, b_i, c_i, d_i,n_up)
 !    
 !   ------- coef. calc. ---------
     do n=2, nmax-1
-!
-        delt_x_e = xscl(n+1)-xscl(n)
-        delt_x_w = xscl(n)-xscl(n-1)
-        delt_xc_e= xvel(n)-xscl(n)
-        delt_xc_w= xscl(n)-xvel(n-1)
-        Gamma_e  = (delt_xc_e/delt_x_e)*(Gamma(n+1)-Gamma(n))+Gamma(n)
-        Gamma_w  = (delt_xc_w/delt_x_w)*(Gamma(n)-Gamma(n-1))+Gamma(n-1)
+
+        delta_x_E = xvel(n+1) - xvel(n)
+        delta_x_P = xvel(n)   - xvel(n-1)
 
         if (is_flat .eqv. .true.) then
             ! flat flame
             ! Control Volume is Quadrangular prism
-            S_e      = 1.0d0 ! area of upper surface of Control Volume
-            S_w      = 1.0d0 ! area of lower surface of Control Volume
-            V_p      = xvel(n) - xvel(n-1) ! volume of Control Volume
+            S_E  = 1.0d0 ! surface area of upper CV
+            S_P  = 1.0d0 ! surface area of lower CV
+            S_se = 1.0d0 ! surface area of center CV
+            V_e  = xscl(n+1) - xscl(n) ! Volume of CV
         else 
             ! spherical flame
             ! Control Volume is Spherical shell
-            S_e      = 4.0d0*pai*xvel(n)**2   ! area of upper surface of Control Volume
-            S_w      = 4.0d0*pai*xvel(n-1)**2 ! area of lower surface of Control Volume
-            V_p      = (4.0d0/3.0d0)*pai*(xvel(n)**3-xvel(n-1)**3) ! volume of Control Volume
+            S_E      = 4.0d0*pai*xscl(n+1)**2   ! area of upper surface of Control Volume
+            S_P      = 4.0d0*pai*xscl(n)**2 ! area of lower surface of Control Volume
+            S_se     = 4.0d0*pai*xvel(n)**2 ! area of center surface of Control Volume
+            V_e      = (4.0d0/3.0d0)*pai*(xscl(n+1)**3-xscl(n)**3) ! volume of Control Volume
         endif
-!
-        F_e = ((delt_xc_e/delt_x_e)*(dens(n+1)-dens(n))+dens(n))  *vel(n)    *S_e
-        F_w = ((delt_xc_w/delt_x_w)*(dens(n)-dens(n-1))+dens(n-1))*vel(n-1)  *S_w
-        D_e = Gamma_e/delt_x_e                                               *S_e
-        D_w = Gamma_w/delt_x_w                                               *S_w
-        Pe_e =(F_e/S_e)*(xvel(n)-xvel(n-1))/Gamma_e
-        Pe_w =(F_w/S_w)*(xvel(n)-xvel(n-1))/Gamma_w
-!
+
+        F_E = dens(n+1)*0.5d0*(vel(n+1) + vel(n))*S_E ! dens_E*vel_E*S_E
+        F_P = dens(n)  *0.5d0*(vel(n-1) + vel(n))*S_E ! dens_P*vel_P*S_P
+        D_E = Gamma(n+1)/delta_x_E*S_E                ! Gamma_E/(delta_x_E)*S_E
+        D_P = Gamma(n)  /delta_x_P*S_P                ! Gamma_P/(delta_x_P)*S_P
+
+        ! Peclet number        
+        Pe_E = F_E/D_E 
+        Pe_P = F_P/D_P
+        
+        ! function A(|P|) for convection and diffusion
+        ! see Patanker Table 5.2
         if (n_up.eq.1) then
-            APe_e = 1.0d0
-            APe_w = 1.0d0
+            APe_E = 1.0d0
+            APe_P = 1.0d0
         else if (n_up.eq.2) then
-            APe_e = ddim(1.0d0-0.5d0*dabs(Pe_e),0.0d0)
-            APe_w = ddim(1.0d0-0.5d0*dabs(Pe_w),0.0d0)
+            APe_E = ddim(1.0d0-0.5d0*dabs(Pe_E),0.0d0)
+            APe_P = ddim(1.0d0-0.5d0*dabs(Pe_P),0.0d0)
         else if (n_up.eq.3) then
-            APe_e = ddim((1.0d0-0.1d0*dabs(Pe_e))**5,0.0d0)
-            APe_w = ddim((1.0d0-0.1d0*dabs(Pe_w))**5,0.0d0)
+            APe_E = ddim((1.0d0-0.1d0*dabs(Pe_E))**5,0.0d0)
+            APe_P = ddim((1.0d0-0.1d0*dabs(Pe_P))**5,0.0d0)
         else
-            APe_e = 1.0d0-0.5d0*dabs(Pe_e)
-            APe_w = 1.0d0-0.5d0*dabs(Pe_w)
+            APe_E = 1.0d0-0.5d0*dabs(Pe_E)
+            APe_P = 1.0d0-0.5d0*dabs(Pe_P)
         end if
-        b_i(n) = D_e*APe_e+ddim(-F_e,0.0d0)
-        c_i(n) = D_w*APe_w+ddim(F_w,0.0d0)
-        a_i(n) = (dens(n)/delt_t)*V_p+b_i(n)+c_i(n)+(F_e-F_w)
-        d_i(n) = (phi(n)*o_dens(n)/delt_t)*V_p
+
+        ! coefficients of discretized form in CV
+        ! see Patanakar 5.3-2
+        a_f = D_E*APe_E+ddim(-F_E,0.0d0)
+        a_p = D_P*APe_P+ddim(F_P,0.0d0)
+        o_a_e = o_dens_e*V_e/delta_t
+        b     = o_a_e*phi(n)
+        a_e   = a_f + a_e + o_a_e
+        
+        ! coefficients for TDMA
+        a_i(n) = a_e
+        b_i(n) = a_f
+        c_i(n) = a_p
+        d_i(n) = b + (p_star(n) - p_star(n+1))*S_se ! b + (p_star_P - p_star_E)*S_se 
 !
     end do
 !
