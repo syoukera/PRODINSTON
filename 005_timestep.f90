315a458f182f333c8cb2fe721257e704fd40d60b
syoukera <s.akira2986@gmail.com>
2023-07-13 14:05:21 +0900

    [005_timestep] Imprement conversion loop

--- 005_timestep.f90
+++ 005_timestep.f90
@@ -1,46 +1,58 @@
 subroutine timestep_simple
 
+    use main_variables
     implicit none
+
+    real*8 b_sum
+    integer i
 
     ! SIMPLE algorism
     ! see Patankar p.130
 
-    ! step 1
-    ! estimate p_star (already done by step 7)
-    ! p_star = p
+    ! set initial mass generation term
+    b_sum = 1.0d0
 
-    ! step 2
-    ! get u_star from momentum equation
-    call vel_trans()
+    ! conversion loop 
+    ! until mass generation term b => 0.0
+    do while (b_sum < 1.0d-8)
 
-    ! step 3
-    ! get p_dash from eq. (6.22)
-    ! pressure correction equation
-    call pres_correct()
+        ! step 1
+        ! estimate p_star (already done by step 7)
+        ! p_star = p
 
-    ! check conversion
-    call calc_mass_generation_term(b)
-    if (b < 1.0d-8) then 
-        exit
-    endif 
+        ! step 2
+        ! get u_star from momentum equation
+        call vel_trans()
 
-    call get_p_dash(p_dash, u_star)
+        ! step 3
 
-    ! step 4
-    ! get p (with relaxation 0.8)
-    p = p_dash + 0.8d0*p_star
+        ! get p_dash from eq. (6.22)
+        ! pressure correction equation
+        call pres_correct()
 
-    ! step 5
-    ! get u from correction equation of velocity
-    call get_u(u, p)
+        ! summation mass generation term
+        b_sum = 0.0d0
+        do i = 2, nmax-1
+            b_sum = b_sum + b_mass(i)
+        enddo
+        
+        ! step 4
+        ! get p (with relaxation 0.8)
+        p = p_star + 0.8d0*p_dash
 
-    ! step 6
-    ! update other variables
-    call update_temp()
-    call update_chsp()
+        ! step 5
+        ! get u from correction equation of velocity
+        call get_u(u, p)
 
-    ! step 7
-    ! set p as p_star
-    p_star = p
+        ! step 6
+        ! update other variables
+        call update_temp()
+        call update_chsp()
+
+        ! step 7
+        ! set p as p_star
+        p_star = p
+
+    enddo
 
 end subroutine timestep_simple
